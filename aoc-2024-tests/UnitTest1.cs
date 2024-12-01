7b1d5fc7f96254f1093f79c9e24fcb0957ed661b
Nick Van Den Broeck <37626327+NickVanDenBroeck@users.noreply.github.com>
2024-12-01 09:19:37 +0100

    Add test project

---
+++ aoc-2024-tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace aoc_2024_tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
