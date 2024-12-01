046278c56a6e58ff8f19a97c5a2dbf9ed95c8e66
Nick Van Den Broeck <37626327+NickVanDenBroeck@users.noreply.github.com>
2024-12-01 12:14:11 +0100

    Add Day 2 structure

---
+++ aoc-2024.Tests/Day2/Day2Should.cs
@@ -0,0 +1,15 @@
+﻿namespace aoc_2024.Tests;
+
+public class Day2Should
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
