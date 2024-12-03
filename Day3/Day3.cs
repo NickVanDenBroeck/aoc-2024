8fee47c4ba601d0ecce1f3867678515c57fdec02
Nick Van Den Broeck <37626327+NickVanDenBroeck@users.noreply.github.com>
2024-12-03 07:09:50 +0100

    Day 3 Setup

---
+++ Day3/Day3.cs
@@ -0,0 +1,17 @@
+﻿namespace aoc_2024.Day3;
+
+internal class Day3
+{
+    public static void Problem1()
+    {
+        string[] input = System.IO.File.ReadAllLines(".\\Day3\\Input.txt");
+        Console.WriteLine($"Day 3 Part 1:");
+    }
+
+    public static void Problem2()
+    {
+        string[] input = System.IO.File.ReadAllLines(".\\Day3\\Input.txt");
+        Console.WriteLine($"Day 3 Part 1:");
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,8 +1,12 @@
 ﻿using Day1Solution = aoc_2024.Day1.Day1;
 using Day2Solution = aoc_2024.Day2.Day2;
+using Day3Solution = aoc_2024.Day3.Day3;
 
 Day1Solution.Problem1();
 Day1Solution.Problem2();
 
 Day2Solution.Problem1();
 Day2Solution.Problem2();
+
+Day3Solution.Problem1();
+Day3Solution.Problem2();
