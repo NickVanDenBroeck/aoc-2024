2cab865ad267f07e02bcf2897af7eb159d204832
Nick Van Den Broeck <37626327+NickVanDenBroeck@users.noreply.github.com>
2024-12-03 07:34:26 +0100

    Day 3 Solution

--- Day3/Day3.cs
+++ Day3/Day3.cs
@@ -1,17 +1,81 @@
-﻿namespace aoc_2024.Day3;
+﻿using System.Text.RegularExpressions;
+
+namespace aoc_2024.Day3;
 
 internal class Day3
 {
     public static void Problem1()
     {
-        string[] input = System.IO.File.ReadAllLines(".\\Day3\\Input.txt");
-        Console.WriteLine($"Day 3 Part 1:");
+        string input = File.ReadAllText(".\\Day3\\Input.txt");
+        int totalSum = SumofMultiplications(input);
+
+        Console.WriteLine($"Day 3 Part 1: {totalSum}");
     }
 
     public static void Problem2()
     {
-        string[] input = System.IO.File.ReadAllLines(".\\Day3\\Input.txt");
-        Console.WriteLine($"Day 3 Part 1:");
+        string input = File.ReadAllText(".\\Day3\\Input.txt");
+        int totalSum = CalculateEnabledMultiplicationSum(input);
+
+        Console.WriteLine($"Day 3 Part 2: {totalSum}");
+    }
+
+
+    public static int SumofMultiplications(string input)
+    {
+        string pattern = @"mul\((\d{1,3}),(\d{1,3})\)";
+
+        MatchCollection matches = Regex.Matches(input, pattern);
+
+        int totalSum = 0;
+
+        foreach (Match match in matches)
+        {
+            int x = int.Parse(match.Groups[1].Value);
+            int y = int.Parse(match.Groups[2].Value);
+            totalSum += x * y;
+        }
+
+        return totalSum;
+    }
+
+
+    public static int CalculateEnabledMultiplicationSum(string input)
+    {
+        string mulPattern = @"mul\((\d{1,3}),(\d{1,3})\)";
+        string doPattern = @"do\(\)";
+        string dontPattern = @"don't\(\)";
+
+        string combinedPattern = $"{mulPattern}|{doPattern}|{dontPattern}";
+
+        MatchCollection matches = Regex.Matches(input, combinedPattern);
+
+        bool isEnabled = true;
+        int totalSum = 0;
+
+        // Process each match
+        foreach (Match match in matches)
+        {
+            if (match.Value.StartsWith("do()"))
+            {
+                isEnabled = true;
+            }
+            else if (match.Value.StartsWith("don't()"))
+            {
+                isEnabled = false;
+            }
+            else if (match.Value.StartsWith("mul"))
+            {
+                if (isEnabled)
+                {
+                    int x = int.Parse(match.Groups[1].Value);
+                    int y = int.Parse(match.Groups[2].Value);
+                    totalSum += x * y;
+                }
+            }
+        }
+
+        return totalSum;
     }
 }
 
