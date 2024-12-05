2e5243862f5c6f3afe0c73c5d8bcd0bd6f15cf96
Nick Van Den Broeck <nick.van.den.broeck@lansweeper.com>
2024-12-05 08:23:30 +0100

    Day 5 Setup

---
+++ Day5/Day5.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aoc_2024.Day5
+{
+    internal class Day5
+    {
+    }
+}
