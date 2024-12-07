9c034b0eb2308115fbbbb45bd0f1618c736aec2e
Nick Van Den Broeck <37626327+NickVanDenBroeck@users.noreply.github.com>
2024-12-07 22:26:06 +0100

    Day 5 Solution

--- Day5/Day5.cs
+++ Day5/Day5.cs
@@ -1,12 +1,164 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace aoc_2024.Day5;
 
-namespace aoc_2024.Day5
+internal class Day5
 {
-    internal class Day5
+    public static void Problem1()
     {
+        string[] input = File.ReadAllLines(".\\Day5\\input.txt");
+
+        var rules = new List<(int, int)>();
+        var updates = new List<List<int>>();
+        bool isRuleSection = true;
+
+        foreach (var line in input)
+        {
+            if (string.IsNullOrWhiteSpace(line))
+            {
+                isRuleSection = false;
+                continue;
+            }
+
+            if (isRuleSection)
+            {
+                var parts = line.Split('|').Select(int.Parse).ToArray();
+                rules.Add((parts[0], parts[1]));
+            }
+            else
+            {
+                var update = line.Split(',').Select(int.Parse).ToList();
+                updates.Add(update);
+            }
+        }
+
+        var ruleLookup = rules.ToLookup(r => r.Item1, r => r.Item2);
+        var validMiddlePages = new List<int>();
+
+        foreach (var update in updates)
+        {
+            if (IsValidUpdate(update, ruleLookup))
+            {
+                validMiddlePages.Add(update[update.Count / 2]);
+            }
+        }
+
+        int result = validMiddlePages.Sum();
+
+        Console.WriteLine("Day 5 Part 1: The sum of the middle page numbers is " + result);
+    }
+
+    static bool IsValidUpdate(List<int> update, ILookup<int, int> ruleLookup)
+    {
+        var pagePositions = update
+            .Select((page, index) => new { Page = page, Position = index })
+            .ToDictionary(x => x.Page, x => x.Position);
+
+        foreach (var rule in ruleLookup)
+        {
+            int x = rule.Key;
+            foreach (int y in rule)
+            {
+                if (pagePositions.ContainsKey(x) && pagePositions.ContainsKey(y))
+                {
+                    if (pagePositions[x] >= pagePositions[y])
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+
+
+
+    public static void Problem2()
+    {
+        string[] input = File.ReadAllLines(".\\Day5\\input.txt");
+
+        var rules = new List<(int, int)>();
+        var updates = new List<List<int>>();
+        bool isRuleSection = true;
+
+        foreach (var line in input)
+        {
+            if (string.IsNullOrWhiteSpace(line))
+            {
+                isRuleSection = false;
+                continue;
+            }
+
+            if (isRuleSection)
+            {
+                var parts = line.Split('|').Select(int.Parse).ToArray();
+                rules.Add((parts[0], parts[1]));
+            }
+            else
+            {
+                var update = line.Split(',').Select(int.Parse).ToList();
+                updates.Add(update);
+            }
+        }
+
+        var ruleLookup = rules.ToLookup(r => r.Item1, r => r.Item2);
+        var reorderedMiddlePages = new List<int>();
+
+        foreach (var update in updates)
+        {
+            if (!IsValidUpdate(update, ruleLookup))
+            {
+                var reordered = ReorderUpdate(update, ruleLookup);
+                reorderedMiddlePages.Add(reordered[reordered.Count / 2]);
+            }
+        }
+
+        int result = reorderedMiddlePages.Sum();
+        Console.WriteLine($"Day 5 Part 2: Sum of reordered middle pages is {result}");
+    }
+
+    static List<int> ReorderUpdate(List<int> update, ILookup<int, int> ruleLookup)
+    {
+        var graph = new Dictionary<int, List<int>>();
+        var inDegree = new Dictionary<int, int>();
+
+        foreach (int page in update)
+        {
+            graph[page] = new List<int>();
+            inDegree[page] = 0;
+        }
+
+        foreach (var rule in ruleLookup)
+        {
+            int x = rule.Key;
+            foreach (int y in rule)
+            {
+                if (update.Contains(x) && update.Contains(y))
+                {
+                    graph[x].Add(y);
+                    inDegree[y]++;
+                }
+            }
+        }
+
+        var sorted = new List<int>();
+        var queue = new Queue<int>(inDegree.Where(kvp => kvp.Value == 0).Select(kvp => kvp.Key));
+
+        while (queue.Count > 0)
+        {
+            int current = queue.Dequeue();
+            sorted.Add(current);
+
+            foreach (var neighbor in graph[current])
+            {
+                inDegree[neighbor]--;
+                if (inDegree[neighbor] == 0)
+                {
+                    queue.Enqueue(neighbor);
+                }
+            }
+        }
+
+        return sorted;
     }
 }
+
--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using Day2Solution = aoc_2024.Day2.Day2;
 using Day3Solution = aoc_2024.Day3.Day3;
 using Day4Solution = aoc_2024.Day4.Day4;
+using Day5Solution = aoc_2024.Day5.Day5;
 
 Day1Solution.Problem1();
 Day1Solution.Problem2();
@@ -15,3 +16,5 @@ Day3Solution.Problem2();
 Day4Solution.Problem1();
 Day4Solution.Problem2();
 
+Day5Solution.Problem1();
+Day5Solution.Problem2();
